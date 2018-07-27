4f7f1f091ae3cb5e30cdfc4bec7ab29564a2821b
Cweili <3weili@gmail.com>
2018-07-27 10:35:26 +0800

    release: v2.0.3

--- dist/req-json.wx.js
+++ dist/req-json.wx.js
@@ -1,197 +1,191 @@
 /*!
  * req-json by @Cweili - https://github.com/Cweili/req-json
  */
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global.ReqJSON = factory());
-}(this, (function () { 'use strict';
+'use strict';
 
-  var encode = encodeURIComponent;
+var encode = encodeURIComponent;
 
-  function isObject(value) {
-    var type = typeof value;
-    return value != null && (type === 'object' || type === 'function');
+function isObject(value) {
+  var type = typeof value;
+  return value != null && (type === 'object' || type === 'function');
+}
+
+function isFunction(value) {
+  if (!isObject(value)) {
+    return false;
+  }
+  // The use of `Object#toString` avoids issues with the `typeof` operator
+  // in Safari 9 which returns 'object' for typed arrays and other constructors.
+  var tag = Object.prototype.toString.call(value);
+  var asyncTag = '[object AsyncFunction]';
+  var funcTag = '[object Function]';
+  var genTag = '[object GeneratorFunction]';
+  var proxyTag = '[object Proxy]';
+  return tag === funcTag || tag === genTag || tag === asyncTag || tag === proxyTag;
+}
+
+function each(collection, handler) {
+  return collection && (Array.isArray(collection) ? collection.forEach(handler) : Object.keys(collection).forEach(function (key) {
+    return handler(collection[key], key);
+  }));
+}
+
+function assign(target) {
+  for (var _len = arguments.length, sources = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    sources[_key - 1] = arguments[_key];
   }
 
-  function isFunction(value) {
-    if (!isObject(value)) {
-      return false;
-    }
-    // The use of `Object#toString` avoids issues with the `typeof` operator
-    // in Safari 9 which returns 'object' for typed arrays and other constructors.
-    var tag = Object.prototype.toString.call(value);
-    var asyncTag = '[object AsyncFunction]';
-    var funcTag = '[object Function]';
-    var genTag = '[object GeneratorFunction]';
-    var proxyTag = '[object Proxy]';
-    return tag === funcTag || tag === genTag || tag === asyncTag || tag === proxyTag;
-  }
-
-  function each(collection, handler) {
-    return collection && (Array.isArray(collection) ? collection.forEach(handler) : Object.keys(collection).forEach(function (key) {
-      return handler(collection[key], key);
-    }));
-  }
-
-  function assign(target) {
-    for (var _len = arguments.length, sources = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-      sources[_key - 1] = arguments[_key];
-    }
-
-    each(sources, function (src) {
-      each(src, function (value, key) {
-        target[key] = value;
-      });
+  each(sources, function (src) {
+    each(src, function (value, key) {
+      target[key] = value;
     });
+  });
 
-    return target;
-  }
+  return target;
+}
 
-  function omit(obj, attrs) {
-    var result = {};
+function omit(obj, attrs) {
+  var result = {};
 
-    each(obj, function (value, key) {
-      if (attrs.indexOf(key) < 0) {
-        result[key] = value;
-      }
-    });
-
-    return result;
-  }
-
-  function transformQuery(args) {
-    return Object.keys(args).sort().map(function (key) {
-      return key + '=' + encode(args[key]);
-    }).join('&');
-  }
-
-  function fillUrl(method, path, data) {
-    var pattern = /\/:(\w+)/g;
-    var variables = [];
-    var isDataObject = isObject(data);
-    var result = path.replace(pattern, function ($0, $1) {
-      variables.push($1);
-      var value = isDataObject ? data[$1] : data;
-      return value != null ? '/' + encode(value) : '';
-    });
-    if (isDataObject && !/POST|PUT/.test(method)) {
-      var query = transformQuery(omit(data, variables));
-      if (query) {
-        result += '?' + query;
-      }
+  each(obj, function (value, key) {
+    if (attrs.indexOf(key) < 0) {
+      result[key] = value;
     }
-    return result;
-  }
+  });
 
-  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+  return result;
+}
 
-  var methods = ['get', 'post', 'put', 'delete'];
+function transformQuery(args) {
+  return Object.keys(args).sort().map(function (key) {
+    return key + '=' + encode(args[key]);
+  }).join('&');
+}
 
-  function req(context, adapter) {
-    return new Promise(function (resolve, reject) {
-      var options = context.options;
-
-      context.header = context.headers = assign({}, options.header, options.headers, context.header, context.headers);
-      adapter(context, resolve, reject);
-    });
-  }
-
-  var ReqJSON = function () {
-    function ReqJSON() {
-      _classCallCheck(this, ReqJSON);
-
-      this.middlewares = [];
+function fillUrl(method, path, data) {
+  var pattern = /\/:(\w+)/g;
+  var variables = [];
+  var isDataObject = isObject(data);
+  var result = path.replace(pattern, function ($0, $1) {
+    variables.push($1);
+    var value = isDataObject ? data[$1] : data;
+    return value != null ? '/' + encode(value) : '';
+  });
+  if (isDataObject && !/POST|PUT/.test(method)) {
+    var query = transformQuery(omit(data, variables));
+    if (query) {
+      result += '?' + query;
     }
+  }
+  return result;
+}
 
-    ReqJSON.setAdapter = function setAdapter(adapter) {
-      ReqJSON.adapter = adapter;
-    };
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    ReqJSON.prototype.resource = function resource(path, options) {
-      var _this = this;
+var methods = ['get', 'post', 'put', 'delete'];
 
-      var fns = {};
-      each(methods, function (method) {
-        fns[method] = function (data, newOptions) {
-          method = method.toUpperCase();
-          var url = fillUrl(method, path, data);
-          var context = {
-            path: path,
-            method: method,
-            url: url,
-            data: data,
-            options: assign({}, options, newOptions)
-          };
-          return _this._dispatch(context, function () {
-            return req(context, ReqJSON.adapter);
-          }).then(function () {
-            return context.response;
-          });
+function req(context, adapter) {
+  return new Promise(function (resolve, reject) {
+    var options = context.options;
+
+    context.header = context.headers = assign({}, options.header, options.headers, context.header, context.headers);
+    adapter(context, resolve, reject);
+  });
+}
+
+var ReqJSON = function () {
+  function ReqJSON() {
+    _classCallCheck(this, ReqJSON);
+
+    this.middlewares = [];
+  }
+
+  ReqJSON.setAdapter = function setAdapter(adapter) {
+    ReqJSON.adapter = adapter;
+  };
+
+  ReqJSON.prototype.resource = function resource(path, options) {
+    var _this = this;
+
+    var fns = {};
+    each(methods, function (method) {
+      fns[method] = function (data, newOptions) {
+        method = method.toUpperCase();
+        var url = fillUrl(method, path, data);
+        var context = {
+          path: path,
+          method: method,
+          url: url,
+          data: data,
+          options: assign({}, options, newOptions)
         };
-      });
-      return fns;
-    };
-
-    ReqJSON.prototype.use = function use(fn) {
-      if (!isFunction(fn)) {
-        throw new TypeError('Middleware must be a function');
-      }
-      this.middlewares.push(fn);
-    };
-
-    ReqJSON.prototype._dispatch = function _dispatch(context, next) {
-      // last called middleware #
-      var middlewares = this.middlewares;
-
-      var index = -1;
-
-      function dispatch(i) {
-        if (i <= index) {
-          return Promise.reject(new Error('next() called multiple times'));
-        }
-        index = i;
-        var fn = middlewares[i];
-        if (i === middlewares.length) {
-          fn = next;
-        }
-        try {
-          return Promise.resolve(fn(context, function () {
-            return dispatch(i + 1);
-          }));
-        } catch (err) {
-          return Promise.reject(err);
-        }
-      }
-
-      return dispatch(0);
-    };
-
-    return ReqJSON;
-  }();
-
-  function wx$1 (context, resolve, reject) {
-    var url = context.url.replace(/\?.*/, '');
-    wx.request({
-      url: url,
-      data: context.data,
-      header: context.header,
-      method: context.method,
-      success: function success(res) {
-        context.headers = context.header = res.header;
-        context.status = res.statusCode;
-        resolve(context.response = res.data);
-      },
-      fail: function fail(res) {
-        context.status = res.statusCode;
-        context.response = res.errMsg;
-        reject(new Error(context.response = res.errMsg));
-      }
+        return _this._dispatch(context, function () {
+          return req(context, ReqJSON.adapter);
+        }).then(function () {
+          return context.response;
+        });
+      };
     });
-  }
+    return fns;
+  };
 
-  ReqJSON.setAdapter(wx$1);
+  ReqJSON.prototype.use = function use(fn) {
+    if (!isFunction(fn)) {
+      throw new TypeError('Middleware must be a function');
+    }
+    this.middlewares.push(fn);
+  };
+
+  ReqJSON.prototype._dispatch = function _dispatch(context, next) {
+    // last called middleware #
+    var middlewares = this.middlewares;
+
+    var index = -1;
+
+    function dispatch(i) {
+      if (i <= index) {
+        return Promise.reject(new Error('next() called multiple times'));
+      }
+      index = i;
+      var fn = middlewares[i];
+      if (i === middlewares.length) {
+        fn = next;
+      }
+      try {
+        return Promise.resolve(fn(context, function () {
+          return dispatch(i + 1);
+        }));
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return dispatch(0);
+  };
 
   return ReqJSON;
+}();
 
-})));
+function wx$1 (context, resolve, reject) {
+  var url = context.url.replace(/\?.*/, '');
+  wx.request({
+    url: url,
+    data: context.data,
+    header: context.header,
+    method: context.method,
+    success: function success(res) {
+      context.headers = context.header = res.header;
+      context.status = res.statusCode;
+      resolve(context.response = res.data);
+    },
+    fail: function fail(res) {
+      context.status = res.statusCode;
+      context.response = res.errMsg;
+      reject(new Error(context.response = res.errMsg));
+    }
+  });
+}
+
+ReqJSON.setAdapter(wx$1);
+
+module.exports = ReqJSON;
